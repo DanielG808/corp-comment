bfdf85b48d43cdd9523ab1991e55a315e85cba39
Dan G <daniel.w.gray1994@gmail.com>
2024-05-21 14:20:19 -0400

    completed practice project

---
+++ src/components/feedback/FeedbackForm.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { MAX_CHARACTERS } from "../../lib/constants";
+
+type FeedbackFormProps = {
+  onAddToList: (text: string) => void;
+};
+
+export default function FeedbackForm({ onAddToList }: FeedbackFormProps) {
+  const [text, setText] = useState("");
+  const [showValidIndicator, setShowValidIndicator] = useState(false);
+  const [showValidInvalidIndicator, setShowValidInvalidIndicator] =
+    useState(false);
+  const charCount = MAX_CHARACTERS - text.length;
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    if (newText.length > MAX_CHARACTERS) {
+      return;
+    }
+    setText(newText);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (text.includes("#") && text.length >= 5) {
+      setShowValidIndicator(true);
+      setTimeout(() => {
+        setShowValidIndicator(false);
+      }, 2000);
+    } else {
+      setShowValidInvalidIndicator(true);
+      setTimeout(() => {
+        setShowValidInvalidIndicator(false);
+      }, 2000);
+      return;
+    }
+
+    onAddToList(text);
+    setText("");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={`form ${showValidIndicator ? "form--valid" : null} ${
+        showValidInvalidIndicator ? "form--invalid" : null
+      }`}
+    >
+      <textarea
+        value={text}
+        onChange={handleChange}
+        id="feedback-textarea"
+        placeholder=""
+        spellCheck={false}
+      />
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company
+      </label>
+      <div className="">
+        <p className="u-italic">{charCount}</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  );
+}
---
+++ src/contexts/FeedbackItemsContextProvider.tsx
@@ -0,0 +1,97 @@
+import { createContext, useMemo, useState } from "react";
+import { TFeedbackItem } from "../lib/types";
+import { useFeedbackItems } from "../lib/hooks";
+
+type FeedbackItemsContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type TFeedbackItemsContext = {
+  isLoading: boolean;
+  errorMessage: string;
+  companyList: string[];
+  filteredFeedbackItems: TFeedbackItem[];
+  handleAddToList: (text: string) => void;
+  handleSelectCompany: (company: string) => void;
+};
+
+export const FeedbackItemsContext = createContext<TFeedbackItemsContext | null>(
+  null
+);
+
+export default function FeedbackItemsContextProvider({
+  children,
+}: FeedbackItemsContextProviderProps) {
+  const { feedbackItems, setFeedbackItems, isLoading, errorMessage } =
+    useFeedbackItems();
+  const [selectedCompany, setSelectedCompany] = useState("");
+
+  const companyList = useMemo(
+    () =>
+      feedbackItems
+        .map((item) => item.company)
+        .filter((company, index, array) => {
+          return array.indexOf(company) === index;
+        }),
+    [feedbackItems]
+  );
+
+  const filteredFeedbackItems = useMemo(
+    () =>
+      selectedCompany
+        ? feedbackItems.filter(
+            (feedbackItem) => feedbackItem.company === selectedCompany
+          )
+        : feedbackItems,
+    [feedbackItems, selectedCompany]
+  );
+
+  const handleAddToList = async (text: string) => {
+    const companyName = text
+      .split(" ")
+      .find((word) => word.includes("#"))!
+      .substring(1);
+
+    const newItem: TFeedbackItem = {
+      id: new Date().getTime(),
+      badgeLetter: companyName.substring(0, 1).toUpperCase(),
+      company: companyName,
+      daysAgo: 0,
+      text: text,
+      upvoteCount: 0,
+    };
+
+    setFeedbackItems([...feedbackItems, newItem]);
+
+    await fetch(
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
+      {
+        method: "POST",
+        body: JSON.stringify(newItem),
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+      }
+    );
+  };
+
+  const handleSelectCompany = (company: string) => {
+    setSelectedCompany(company);
+  };
+
+  return (
+    <FeedbackItemsContext.Provider
+      value={{
+        isLoading,
+        errorMessage,
+        companyList,
+        filteredFeedbackItems,
+        handleAddToList,
+        handleSelectCompany,
+      }}
+    >
+      {children}
+    </FeedbackItemsContext.Provider>
+  );
+}
